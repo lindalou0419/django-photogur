612e682ae8bdf75a6c294d1a6e395f3a05248d04
ilyana19 <aries_2152@hotmail.com>
2019-09-16 17:12:54 -0400

    displayed list of pictures

--- photogur/views.py
+++ photogur/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from photogur.models import Picture
 
 def root(request):
   return HttpResponseRedirect('pictures')
 
 def picture(request):
-  response = render(request, 'pictures.html')
+  context = {'pictures': Picture.objects.all()}
+  response = render(request, 'pictures.html', context)
   return HttpResponse(response)
