6f4b9a93081ea57bfb393be8c5110ecd485f7b7d
ilyana19 <aries_2152@hotmail.com>
2019-09-19 19:37:48 -0400

    comment form and redirect after posting

--- photogur/urls.py
+++ photogur/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
   path('admin/', admin.site.urls),
   path('', views.root, name="index"),
   path('pictures/', views.picture),
-  path('pictures/<int:pic_id>', views.picture_show, name='picture_details'),
+  path('pictures/<int:pic_id>/', views.picture_show, name='picture_details'),
   path('search', views.picture_search, name='picture_search'),
   path('comments/new', views.comment_create, name='comment_create'),
 ]
--- photogur/views.py
+++ photogur/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from photogur.models import Picture
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+from django.urls import path, reverse
+from photogur.models import Picture, Comment
+
+# import pdb
 
 def root(request):
   return HttpResponseRedirect('pictures')
@@ -35,5 +39,18 @@ def picture_search(request):
   response = render(request, 'results.html', context)
   return HttpResponse(response)
 
+@require_http_methods(['POST'])
 def comment_create(request):
-  pass
+  name = request.POST['poster-name']
+  message = request.POST['poster-comment']
+  picture_id = request.POST['picture-id']
+  picture = Picture.objects.get(id=picture_id)
+
+  Comment.objects.create(
+    name = name,
+    message = message,
+    picture = picture
+  )
+
+  return redirect('picture_details', picture.id)
+  # return HttpResponseRedirect(reverse('picture_details', args=[picture.id]))
