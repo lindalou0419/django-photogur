eb72e10bf0b76a94e2786f981b9af5c33c3d6948
ilyana19 <aries_2152@hotmail.com>
2019-09-18 21:02:13 -0400

    styled single image details page

--- photogur/urls.py
+++ photogur/urls.py
@@ -4,7 +4,7 @@ from photogur import views
 
 urlpatterns = [
   path('admin/', admin.site.urls),
-  path('', views.root),
+  path('', views.root, name="index"),
   path('pictures/', views.picture),
   path('pictures/<int:pic_id>', views.picture_show, name='picture_details'),
 ]
